64b840d1d2c1a95968f605aaa4e3ab83d8001c2a
agvar <ajeebgeorge@gmail.com>
2025-12-23 09:10:17 -0500

    feature: Add glowing pulse animation

--- components/VoiceButton.tsx
+++ components/VoiceButton.tsx
@@ -1,7 +1,7 @@
 import { BorderRadius, Colors, Spacing, Typography } from '@/constants/theme';
 import { Ionicons } from '@expo/vector-icons';
-import { useState } from 'react';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { useRef, useState } from 'react';
+import { Animated, Pressable, StyleSheet, Text, View } from 'react-native';
 
 interface VoiceButtonProps {
     onRecordingComplete : (text: string) => void;
@@ -9,32 +9,103 @@ interface VoiceButtonProps {
 
 export default function VoiceButton({ onRecordingComplete }: VoiceButtonProps ) {
     const [isRecording, setisRecording] = useState(false);
+    const scaleAnim = useRef(new Animated.Value(1)).current;
+    const opacityAnim = useRef(new Animated.Value(1)).current;
 
     const handlePress= () => {
         if(isRecording) {
             setisRecording(false);
+            stopPulseAnimation();
             onRecordingComplete("This is a test thought");
         }
         else{
             setisRecording(true);
+            startPulseAnimation();
         }
     };
+
+    const startPulseAnimation = () =>{
+        Animated.loop(
+            Animated.parallel([
+                Animated.sequence([
+                    Animated.timing(
+                        scaleAnim,{
+                            toValue: 1.15,
+                            duration: 1000,
+                            useNativeDriver: true
+                        }
+                    ),
+                    Animated.timing(
+                        scaleAnim,{
+                            toValue: 1,
+                            duration: 1000,
+                            useNativeDriver: true
+                        }
+                    )
+                ]),
+                Animated.sequence([
+                    Animated.timing(
+                        opacityAnim,{
+                            toValue: 0.7,
+                            duration: 1000,
+                            useNativeDriver: true
+                        }
+                    ),
+                    Animated.timing(
+                        opacityAnim,{
+                            toValue: 1,
+                            duration: 1000,
+                            useNativeDriver: true
+                        }
+                    )
+                ])
+            ])
+            
+        ).start();
+    };
+
+    const stopPulseAnimation = () =>{
+        scaleAnim.stopAnimation();
+        opacityAnim.stopAnimation();
+
+        Animated.parallel([
+            Animated.timing(
+                scaleAnim,{
+                    toValue: 1,
+                    duration: 200,
+                    useNativeDriver: true
+                }
+            ),
+            Animated.timing(
+                opacityAnim,{
+                    toValue: 1,
+                    duration: 200,
+                    useNativeDriver: true
+                }
+            )
+        ]).start();
+    }
 
     return(
         <View style={styles.container}>
             <Pressable onPress={handlePress}>
                 {({ pressed}) => (
-                    <View style = {[
+                    <Animated.View style = {[
                         styles.button,
                         isRecording && styles.buttonRecording,
                         pressed && styles.buttonPressed,
+                        {
+                            transform: [{scale: scaleAnim}],
+                            opacity: opacityAnim
+
+                        }
                     ]}>
                         <Ionicons
                             name = {isRecording ? "stop-circle-outline" : "mic-outline"}
                             size={40}
                             color="#ffffff"
                         />
-                    </View>
+                    </Animated.View>
                 )}
             </Pressable>
             <Text style={styles.label}>
@@ -56,10 +127,17 @@ const styles = StyleSheet.create({
             borderRadius: BorderRadius.full,
             backgroundColor: Colors.flow,
             justifyContent: 'center',
-            alignItems: 'center'
+            alignItems: 'center',
+            shadowColor: Colors.flow,
+            shadowOffset: {width: 0, height:0},
+            shadowOpacity: 0.5,
+            shadowRadius: 20,
+            elevation: 10
+
         },
         buttonRecording:{
             backgroundColor : Colors.drain,
+            shadowColor: Colors.drain,
         },
         buttonPressed:{
             opacity: 0.8,
