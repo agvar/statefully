e653f9fe098f9c11a70d3f07d53bcddac508e318
agvar <ajeebgeorge@gmail.com>
2025-12-27 23:49:16 -0500

    feature: Add Flatlist for thoughs

--- app/(tabs)/think.tsx
+++ app/(tabs)/think.tsx
@@ -2,35 +2,61 @@ import ThoughtBubble from '@/components/ThoughtBubble';
 import VoiceButton from '@/components/VoiceButton';
 import { Colors, Spacing, Typography } from '@/constants/theme';
 import { Thought } from '@/types';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
 export default function ThinkScreen(){
     //create test data for testing
-    const testThought1:Thought ={
+    const [thoughts,setThoughts] = useState<Thought[]>([
+    {
         id: '1',
         text: 'Had a great idea for project,feels like a breakthrough.',
-        timestamp : new Date(),
+        timestamp : new Date(Date.now()),
         sentiment: 'positive'
-    };
-    const testThought2:Thought ={
-        id: '1',
+    },
+    {
+        id: '2',
         text: 'This has been bummer of a day- I got nothing done.',
-        timestamp : new Date(),
-        sentiment: 'positive'
+        timestamp : new Date(Date.now() - 15 * 60000),
+        sentiment: 'negetive'
+    },
+    {
+        id: '3',
+        text: 'This has been a normal day-nothing special.',
+        timestamp : new Date(Date.now() - 45 * 60000),
+        sentiment: 'neutral'
+    }
+    ]);
+
+    const HandleRecordingComplete = (text: string) =>{
+        const newThought:Thought = {
+            id : Date.now().toString(),
+            text: text,
+            timestamp: new Date(),
+            sentiment: 'neutral'
+        };
+        setThoughts(prevThoughts => [newThought, ...prevThoughts] );
     };
 
     return(
         <View style = {styles.container}>
-            <Text style = {styles.title}>Think Stream</Text>
-            <View style = {styles.content}>
-                <ThoughtBubble thought={testThought1}/>
-                <ThoughtBubble thought={testThought2}/>
-                <VoiceButton 
-                onRecordingComplete={(text) =>{
-                    console.log('Recorded',text)
-                }}/>
-
+            <View style = {styles.header}>
+                <Text style = {styles.title}>Think Stream</Text>
             </View>
+
+            {/* Thought List */}
+        <FlatList
+            data = {thoughts}
+            renderItem = {({ item }) => <ThoughtBubble thought = {item} />}
+            keyExtractor ={item => item.id}
+            contentContainerStyle= {styles.listContent}
+            showsVerticalScrollIndicator = {false}
+            inverted
+        />
+        {/*  Voice Button (Fixed at the Bottom) */}
+        <View style= {styles.VoiceButtonContainer}>
+            <VoiceButton onRecordingComplete={HandleRecordingComplete}/>
+        </View>
         </View>
     );
 }
@@ -39,20 +65,27 @@ const styles = StyleSheet.create({
     container : {
         flex : 1,
         backgroundColor : Colors.background.dark,
-        paddingTop: Spacing['2xl']
+    },
+    header:{
+        paddingTop: Spacing['2xl'] + 20,
+        paddingHorizontal: Spacing.md,
+        paddingBottom: Spacing.md,
+        backgroundColor: Colors.background.dark
     },
     title :{
         fontSize: Typography.size['4xl'],
         fontWeight: Typography.weight.bold,
         color:Colors.text.dark.primary,
-        marginBottom : Spacing.sm,
-        paddingHorizontal: Spacing.md
     },
-    content :{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: Typography.size.base,
-        color:Colors.text.dark.secondary
+    listContent :{
+        paddingBottom: Spacing.md,
+        flexGrow: 1
     },
-})
+    VoiceButtonContainer:{
+        paddingVertical: Spacing.lg,
+        paddingHorizontal:Spacing.md,
+        backgroundColor:Colors.background.dark,
+        borderTopWidth:1,
+        borderTopColor: Colors.border.dark
+    }
+});
