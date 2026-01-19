240a2260f514ce96f41c2931410bb86d58a93ff1
agvar <ajeebgeorge@gmail.com>
2026-01-18 23:54:46 -0500

    feature: add deepgram api key

---
+++ utils/transcription.ts
@@ -0,0 +1,23 @@
+import { File } from 'expo-file-system';
+
+export const transcribeAudio = async(fileUri: string): Promise<string> => {
+    const transcribeUrl = 'https://api.deepgram.com/v1/listen?model=nova-3&smart_format=true'
+    try {
+        const audioFile = new File(fileUri);
+        const audioBuffer = await audioFile.arrayBuffer();
+    const response = await fetch(transcribeUrl,{
+        method : "POST",
+        headers: {
+            "Content-Type" :"audio/wav",
+            "Authorization": "TOKEN API_KEY" ,
+        },
+        body: audioBuffer
+    });
+        const respose_json = await response.json()
+        return respose_json.results.channels[0].alternatives[0].transcript
+    }
+    catch(err){
+        console.error("Transcription failed",err);
+        throw err;
+    }
+}
