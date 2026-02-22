f9e270e4d53bced0f8e5ab2ea8303ab24afea052
ajeebgeorge <ajeebgeorge@gmail.com>
2026-02-21 19:07:35 -0500

    fix: add  ExecutorchModule to get input shapes for decoder, to debug the issue with the decoder's forward function

--- utils/moonshineTranscription.ts
+++ utils/moonshineTranscription.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { TensorPtr, useExecutorchModule, ScalarType ,TokenizerModule} from "react-native-executorch";
+import { TensorPtr, useExecutorchModule, ScalarType ,TokenizerModule, ExecutorchModule} from "react-native-executorch";
 
 interface MoonshineModelHook{
     isReady: boolean;
@@ -33,13 +33,33 @@ export  function useMoonshineModel():MoonshineModelHook {
 
     },[]);
 
+    useEffect(() =>{
+        const debugModel = async() =>{
+            const mod =  new ExecutorchModule();
+            await mod.load(require('../assets/models/moonshine_tiny_xnnpack_decoder.pte'));
+            try {
+                for (let i=0; i<5;i++){
+                    const shape = await mod.getInputShape('forward',i);
+                    console.log(`shape for ${i} input is ${shape}`)
+                }
+
+            } catch(err)
+            {
+                console.log(' no more input slots found');
+            }
+
+        };
+        debugModel();
+    },[]
+    );
+
     const isReady = encoder.isReady && decoder.isReady && istokenizerReady;
     const error = encoder.error?.message || decoder.error?.message;
 
     const transcribe =async (audioData:Float32Array) => {
         console.log("starting transcription process");
-        console.log(`length of audio float32 array ${audioData.length}`)
-        console.log('input sample audio',audioData.slice(0,5));
+        //console.log(`length of audio float32 array ${audioData.length}`)
+        //console.log('input sample audio',audioData.slice(0,5));
         try{
             const audioTensor:TensorPtr ={
                 dataPtr : audioData,
@@ -50,9 +70,9 @@ export  function useMoonshineModel():MoonshineModelHook {
             console.log('encoder input length',[audioTensor].length);
             const encoderOutput = await encoder.forward([audioTensor]);
             const hiddenStateTensor = encoderOutput[0];
-            //console.log('encounter output hidden index 0',hiddenStateTensor);
-            console.log('decoder input',[hiddenStateTensor]);
-
+            console.log('encounter hidden sizes',hiddenStateTensor.sizes);
+            
+            
             let currentToken = [1];
             let transcript = "";
 
@@ -60,14 +80,13 @@ export  function useMoonshineModel():MoonshineModelHook {
             for (let i=0;i<200;i++){
                 const tokenArray = new Int32Array(currentToken);
                 const currentTokenTensor = {
-                dataPtr : tokenArray.buffer,
+                dataPtr : tokenArray,
                 sizes : [1, currentToken.length],
-                scalarType :ScalarType.INT,
+                scalarType :ScalarType.LONG,
                 }
-                console.log("current token for decoder",currentTokenTensor)
-                console.log('Token dataPtr is ArrayBuffer?', currentTokenTensor.dataPtr instanceof ArrayBuffer);
-                console.log('Token dataPtr byteLength:', currentTokenTensor.dataPtr.byteLength);
-                console.log('Expected bytes:', currentToken.length * 4);  // 4 bytes per int32
+                console.log("first token for decoder",currentTokenTensor.sizes)
+                console.log("hidden state encoder",hiddenStateTensor.sizes)
+                console.log('decoder input',[currentTokenTensor, hiddenStateTensor]);
                 
                 const decoderOutput = await decoder.forward([hiddenStateTensor, currentTokenTensor]);
                 const rawBuffer = decoderOutput[0].dataPtr as ArrayBuffer;
