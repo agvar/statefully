dcfa9874d369aef6b479135e9dcdf83a9f0b62de
ajeebgeorge <ajeebgeorge@gmail.com>
2026-03-02 23:20:13 -0500

    fix: updated to remove the .pte assets when bundling

--- metro.config.js
+++ metro.config.js
@@ -5,6 +5,6 @@ const config = getDefaultConfig(__dirname);
 // adding 'pte' to the list of "allowed assets"
 // so Metro knows it should just copy this file into the bundle 
 // rather than trying to read it as code.
-config.resolver.assetExts.push('pte');
+//config.resolver.assetExts.push('pte');
 
 module.exports = config;
