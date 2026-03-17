94ef69d71b8857177f3e2e2051657a02a8307a40
ajeebgeorge <ajeebgeorge@gmail.com>
2026-03-17 08:17:30 -0400

    feature: update gradient to be diagonal

--- app/(tabs)/now.tsx
+++ app/(tabs)/now.tsx
@@ -108,6 +108,8 @@ export default function NowScreen(){
         <View style = {styles.container}>
             <LinearGradient 
                 colors={[Colors.gradient.auroraStart,Colors.background.dark,Colors.gradient.auroraEnd]}
+                start={{ x:0, y:0 }}
+                end={{ x:1, y:1 }} 
                 style={StyleSheet.absoluteFill}
             />
             {/* Header */}
