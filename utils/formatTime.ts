dac22abaa62d5063526ce25517641178aea2f8d2
ajeebgeorge <ajeebgeorge@gmail.com>
2026-04-23 23:52:09 -0400

    feature: add utility for duration calculation

--- utils/formatTime.ts
+++ utils/formatTime.ts
@@ -6,3 +6,18 @@ export const formatTime = (date : Date): string =>{
     const hour = h % 12 || 12 ;
     return `${hour}:${m} ${ampm}`;
 }
+
+
+export const formatDuration = (seconds:number): string =>{
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600)/ 60);
+
+    if (hours > 0){
+        return `${hours}h ${minutes}m`;
+    }
+    if(minutes > 0){
+        return `${minutes}m`
+    }
+    return `${seconds}s`
+};
+
